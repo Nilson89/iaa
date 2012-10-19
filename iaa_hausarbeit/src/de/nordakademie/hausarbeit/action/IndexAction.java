852fdd473f40370c7c356b96c04502d64a3d6834
Nilson89 <niels.maseberg@nordakademie.de>
2012-10-19 14:53:56 +0200

    Struts Tiles eingebunden

---
+++ iaa_hausarbeit/src/de/nordakademie/hausarbeit/action/IndexAction.java
@@ -0,0 +1,18 @@
+package de.nordakademie.hausarbeit.action;
+
+import com.opensymphony.xwork2.Action;
+
+/**
+ * 
+ * @author Niels Maseberg
+ */
+public class IndexAction implements Action {
+
+	/**
+	 * execute
+	 */
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+}
---
+++ iaa_hausarbeit/src/de/nordakademie/hausarbeit/service/ServiceFactory.java
@@ -0,0 +1,16 @@
+package de.nordakademie.hausarbeit.service;
+
+/**
+ * 
+ * @author Niels Maseberg
+ */
+public final class ServiceFactory {
+	
+	/**
+	 * hidden constructor
+	 */
+	private ServiceFactory() {
+	}
+	
+	
+}
